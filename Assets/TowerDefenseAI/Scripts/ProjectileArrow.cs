8de4c46c00ebb283b86cab886ddfddf17a2e190f
hikup100 <hikup100@gmail.com>
2024-05-02 10:11:30 -0400

    new script

--- Assets/TowerDefenseAI/Scripts/ProjectileArrow.cs
+++ Assets/TowerDefenseAI/Scripts/ProjectileArrow.cs
@@ -1,18 +1,51 @@
-﻿/*
+﻿
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ProjectileArrow : MonoBehaviour {
+    public AnimationCurve curve;
+    [SerializeField]
+    private float duration = 1f;
+    [SerializeField]
+    private float heightY = 3f;
 
     public GameObject archerTower;
     public GameObject ennemis;
+
+    public Vector3 start;
+    public Vector3 end;
+
+
     void Start()
     {
         archerTower = GameObject.FindGameObjectWithTag("archerTower");
         ennemis = GameObject.FindGameObjectWithTag("ennemis");
+        start = new Vector3(archerTower.transform.position.x, archerTower.transform.position.y, 0f);
+        end = new Vector3(ennemis.transform.position.x, ennemis.transform.position.y, 0f);
+        
     }
 
+    public IEnumerator Curve(Vector3 start, Vector3 target)
+    {
+        float timePassed = 0f;
+
+        Vector2 end = target;
+
+        while (timePassed < duration)
+        {
+            timePassed+= Time.deltaTime;
+
+            float linearT = timePassed / duration;
+            float heightT = curve.Evaluate(linearT);
+
+            float height = Mathf.Lerp(0f, heightY, heightT);
+
+            transform.position = Vector2.Lerp(start, end, linearT)+ new Vector2(0f, height);
+
+            yield return null;
+        }
+    }
 
     private void Update() {
 
@@ -22,4 +55,4 @@ public class ProjectileArrow : MonoBehaviour {
         }
     }
 
-}*/
+}
